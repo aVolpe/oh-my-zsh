1f8f66e58b1f5f8820591468850252073099eebb
Arturo Volpe <arturovolpe@gmail.com>
2016-06-06 14:00:57 -0400

    Allow change colors in tmux

--- custom/alias_konsole.zsh
+++ custom/alias_konsole.zsh
@@ -1,13 +1,23 @@
 function runwithprofile(){
 
     DEFAULT_SCHEME=`grep ColorScheme ~/.kde4/share/apps/konsole/Shell.profile | cut -d = -f2`
-    konsoleprofile colors="$1" 
+    if [[ -z "$TMUX" ]]
+    then
+        konsoleprofile colors="$1"
+    else
+        printf '\033Ptmux;\033\033]50;%s\007\033\\' colors="$1"
+    fi
     if [[ -n "$3" ]]; then
         sudo $2
     else
         $2
     fi
-    konsoleprofile colors=$DEFAULT_SCHEME
+    if [[ -z "$TMUX" ]]
+    then
+        konsoleprofile colors="$DEFAULT_SCHEME"
+    else
+        printf '\033Ptmux;\033\033]50;%s\007\033\\' colors="$DEFAULT_SCHEME"
+    fi
 }
 
 alias cmus='runwithprofile "Harper" "cmus"'
